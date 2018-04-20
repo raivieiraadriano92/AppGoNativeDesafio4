83cb2aa35171e9ed1ab5421be8ad1a6454e17c66
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2018-04-20 17:36:58 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import 'config/DevToolsConfig';
 import 'config/ReactotronConfig';
@@ -7,8 +8,10 @@ import store from 'store';
 import Routes from 'routes';
 
 const App = () => (
-  <Provider store={store}>
-    <Routes />
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <Routes />
+    </PersistGate>
   </Provider>
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 import sagas from './sagas';
 import reducers from './ducks';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: [], // Lista do que você não quer
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
@@ -12,8 +25,9 @@ const middleware = [
 ];
 
 const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
-const store = createAppropriateStore(reducers, applyMiddleware(...middleware));
+const store = createAppropriateStore(persistedReducer, applyMiddleware(...middleware));
+const persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
-export default store;
+export default { store, persistor };
